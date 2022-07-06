a1d7114bc1e9afd7a0a7afa77a6f06ee47b0bcf6
AnthonyNocom <anthony_nocom@dlsu.edu.ph>
2022-07-07 00:04:49 +0800

    refactored all /Images strings to /images

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def upload_image():
 
 @app.route('/display/<filename>')
 def display_image(filename):
-    return redirect(url_for('static', filename = '/Images/'+filename),code=301)
+    return redirect(url_for('static', filename = '/images/'+filename),code=301)
